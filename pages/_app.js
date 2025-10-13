f558bff33796380103435eafe8780fcbb01c07e4
benz02r <ryanbains2002@gmail.com>
2025-10-13 16:16:49 +0100

    mailchimp and facebook metapixel integration

--- pages/_app.js
+++ pages/_app.js
@@ -1,35 +1,35 @@
 import Head from "next/head";
 import Script from "next/script";
-import "../styles/globals.css"; // Import Tailwind globally
+import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
     return (
         <>
             <Head>
-                {/* Material Icons CDN for icons */}
+                {/* Material Icons */}
                 <link
                     rel="stylesheet"
                     href="https://fonts.googleapis.com/icon?family=Material+Icons"
                 />
             </Head>
 
-            {/* Meta Pixel Code */}
+            {/* Meta Pixel Code (Facebook) */}
             <Script
                 id="facebook-pixel"
                 strategy="afterInteractive"
                 dangerouslySetInnerHTML={{
                     __html: `
-                        !function(f,b,e,v,n,t,s)
-                        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                        n.queue=[];t=b.createElement(e);t.async=!0;
-                        t.src=v;s=b.getElementsByTagName(e)[0];
-                        s.parentNode.insertBefore(t,s)}(window, document,'script',
-                        'https://connect.facebook.net/en_US/fbevents.js');
-                        fbq('init', '3039279902900287');
-                        fbq('track', 'PageView');
-                    `,
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '3039279902900287');
+            fbq('track', 'PageView');
+          `,
                 }}
             />
             <noscript>
@@ -40,7 +40,21 @@ export default function MyApp({ Component, pageProps }) {
                     src="https://www.facebook.com/tr?id=3039279902900287&ev=PageView&noscript=1"
                 />
             </noscript>
-            {/* End Meta Pixel Code */}
+
+            {/*Mailchimp Integration Code */}
+            <Script
+                id="mailchimp"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+            !function(c,h,i,m,p){
+              m=c.createElement(h),p=c.getElementsByTagName(h)[0],
+              m.async=1,m.src=i,p.parentNode.insertBefore(m,p)
+            }(document,"script",
+            "https://chimpstatic.com/mcjs-connected/js/users/9d8e8ae2f4c2b7f6d37106745/0134608e26e5b8c6e0359ec06.js");
+          `,
+                }}
+            />
 
             <Component {...pageProps} />
         </>
