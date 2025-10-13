70666b4b5cedeea05aad3ffe803123fb7026e27c
benz02r <ryanbains2002@gmail.com>
2025-10-13 22:37:40 +0100

    new fixes

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,7 @@ export default function MyApp({ Component, pageProps }) {
                 />
             </Head>
 
-            {/* Meta Pixel Code (Facebook) */}
+            {/* 📊 Facebook Pixel Code */}
             <Script
                 id="facebook-pixel"
                 strategy="afterInteractive"
@@ -32,30 +32,25 @@ export default function MyApp({ Component, pageProps }) {
           `,
                 }}
             />
+
             <noscript>
                 <img
                     height="1"
                     width="1"
                     style={{ display: "none" }}
                     src="https://www.facebook.com/tr?id=3039279902900287&ev=PageView&noscript=1"
+                    alt="facebook pixel"
                 />
             </noscript>
 
-            {/*Mailchimp Integration Code */}
+            {/* 📨 Mailchimp Connected Sites */}
             <Script
                 id="mailchimp"
                 strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `
-            !function(c,h,i,m,p){
-              m=c.createElement(h),p=c.getElementsByTagName(h)[0],
-              m.async=1,m.src=i,p.parentNode.insertBefore(m,p)
-            }(document,"script",
-            "https://chimpstatic.com/mcjs-connected/js/users/9d8e8ae2f4c2b7f6d37106745/0134608e26e5b8c6e0359ec06.js");
-          `,
-                }}
+                src="https://chimpstatic.com/mcjs-connected/js/users/9d8e8ae2f4c2b7f6d37106745/0134608e26e5b8c6e0359ec06.js"
             />
 
+            {/* Render All Pages */}
             <Component {...pageProps} />
         </>
     );
