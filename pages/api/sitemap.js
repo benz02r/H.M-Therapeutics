77128da3fe55a3dbc4acc58a4697a9c6b500df24
benz02r <ryanbains2002@gmail.com>
2026-03-06 23:15:22 +0000

    v2 fixes

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -2,27 +2,27 @@ export default function handler(req, res) {
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         <url>
-            <loc>https://yourdomain.com/</loc>
+            <loc>https://www.hmtherapeutics.co.uk/</loc>
             <priority>1.0</priority>
             <changefreq>daily</changefreq>
         </url>
         <url>
-            <loc>https://yourdomain.com/about</loc>
+            <loc>https://www.hmtherapeutics.co.uk/about</loc>
             <priority>0.8</priority>
             <changefreq>monthly</changefreq>
         </url>
         <url>
-            <loc>https://yourdomain.com/contact</loc>
+            <loc>https://www.hmtherapeutics.co.uk/contact</loc>
             <priority>0.7</priority>
             <changefreq>monthly</changefreq>
         </url>
         <url>
-            <loc>https://yourdomain.com/services</loc>
+            <loc>https://www.hmtherapeutics.co.uk/services</loc>
             <priority>0.8</priority>
             <changefreq>monthly</changefreq>
         </url>
         <url>
-            <loc>https://yourdomain.com/policy</loc>
+            <loc>https://www.hmtherapeutics.co.uk/policy</loc>
             <priority>0.6</priority>
             <changefreq>yearly</changefreq>
         </url>
